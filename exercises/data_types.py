b061a59f8e42f69d5a859750ae4b784793add054
chinmayeeph <chinmayeeph@gmail.com>
2026-09-05 00:27:37 +0530

    Remove duplicate files

--- exercises/data_types.py
+++
@@ -1,6 +0,0 @@
-name = "Chinmayee"              #str
-age = 25                        #int
-height = 5.5                    #float
-city = "Bengaluru"              #str
-favorite_language = "Python"    #str
-is_student = True               #bool
--- exercises/variables.py
+++
@@ -1,5 +0,0 @@
-name = "Chinmayee"
-age = 25
-height = 5.5
-city = "Bengaluru"
-favorite_language = "Python"
