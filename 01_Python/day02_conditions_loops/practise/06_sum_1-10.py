f26d71b7b1b06c7232a31becdb24288289bf42c4
Chinmayee P H <chinmayeeph@gmail.com>
2026-09-10 01:16:03 +0530

    Delete 01_Python/day02_conditions_loops/practise/06_sum_1-10.py

--- 01_Python/day02_conditions_loops/practise/06_sum_1-10.py
+++
@@ -1,6 +0,0 @@
-print("-----Exercise 6 — Sum 1–100-----")      # This program calculates the sum of numbers from 1 to 100.
-# Initialize a variable to store the sum
-sum = 0
-for i in range(1, 101):      # Use a for loop to iterate over the range of numbers from 1 to 100 (inclusive).
-    sum += i      # Add the current number to the sum.
-print("The sum of numbers from 1 to 100 is:", sum)      # Print the final sum after the loop has completed.
