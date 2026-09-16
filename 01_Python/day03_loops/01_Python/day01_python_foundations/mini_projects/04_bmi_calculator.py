0055705fad14a43b6cfeedf1bbc4d87825254cb2
Chinmayee P H <chinmayeeph@gmail.com>
2026-09-16 20:01:03 +0530

    Delete 01_Python/day03_loops/01_Python/day01_python_foundations/mini_projects/04_bmi_calculator.py

--- 01_Python/day03_loops/01_Python/day01_python_foundations/mini_projects/04_bmi_calculator.py
+++
@@ -1,4 +0,0 @@
-weight = int(input("Enter the weight in kgs: "))
-height = float(input("Enter the height in ft: "))
-BMI = weight / height * 2
-print(f"According to your weight and height, your BMI is {BMI}")
